6cc0d08029938b24692439d058c2531e8a7221ea
Caroline Santos de Abreu <caroline@pop-os.localdomain>
2023-09-27 13:45:34 -0300

    header done

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,13 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+import commonStyle from '../../styles/common.module.scss';
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <div className={`${commonStyle.container} ${styles.header}`}>
+      <Link href="/">
+        <img src="/Logo.svg" alt="logo" />
+      </Link>
+    </div>
+  );
 }
